32fe21038a75a3f6da911c92247e422885cb521b
Thomas Voss <thomasvoss@live.com>
2021-12-01 08:55:41 +0100

    Make use of the magic zip function

--- 2021/01/puzzles.py
+++ 2021/01/puzzles.py
@@ -4,10 +4,7 @@ def main() -> None:
 	with open("input", "r") as f:
 		nums = [int(n) for n in f.readlines()]
 
-	# START PART 2
-	nums = [nums[i] + nums[i - 1] + nums[i - 2] for i in range(2, len(nums))]
-	# END PART 2
-	print(sum(nums[i] > nums[i - 1] for i in range(1, len(nums))))
+	print(sum(n < m for n, m in zip(nums, nums[DELTA:])))
 
 if __name__ == "__main__":
 	main()
