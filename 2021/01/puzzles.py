4bc4482a1a2c2e5b652d1fd6163ef5d0b26365e7
Thomas Voss <thomasvoss@live.com>
2021-12-02 21:00:31 +0100

    Specify file encoding

--- 2021/01/puzzles.py
+++ 2021/01/puzzles.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
+
 def main() -> None:
-	with open("input", "r") as f:
+	with open("input", "r", encoding="utf-8") as f:
 		nums = [int(n) for n in f.readlines()]
 
 	print(sum(n < m for n, m in zip(nums, nums[DELTA:])))
+
 
 if __name__ == "__main__":
 	main()
