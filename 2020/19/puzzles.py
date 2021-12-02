e257eb53359927c537d2e9c31ca8eae7a0f6dfe7
Thomas Voss <thomasvoss@live.com>
2021-12-02 13:58:09 +0100

    Combine parts 1 and 2 into one file

--- 2020/19/puzzles.py
+++ 2020/19/puzzles.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 from collections import OrderedDict
+from itertools import product
+
+# START PART 2
 from re import search
 
 
@@ -28,8 +31,11 @@ def string_divide(string: str, div: int) -> list[str]:
 	return l
 
 
+# END PART 2
+
+
 def main() -> None:
-	with open("input", "r") as f:
+	with open("input", "r", encoding="utf-8") as f:
 		data = f.readlines()
 
 	i = 0
@@ -38,10 +44,7 @@ def main() -> None:
 		rules.append(tuple(x for x in tuple(data[i].split(": "))))
 		i += 1
 
-	tests: list[str] = []
-	for line in data:
-		tests.append(line.strip())
-
+	tests = [line.strip() for line in data]
 	patterns: dict[str, list[str]] = {}
 
 	# Get "a" and "b" out of the way to do less comparisons in the next loop. Also make use of
@@ -57,27 +60,29 @@ def main() -> None:
 				continue
 
 			res = rule[1].split(" | ")
-			req_rules = list(OrderedDict.fromkeys(" ".join(res).split(" ")))
-			all_in = True
-			for req in req_rules:
-				if req not in patterns:
-					all_in = False
-					break
-			if all_in == True:
+			if all(req in patterns for req in list(OrderedDict.fromkeys(" ".join(res).split(" ")))):
 				all_combos: list[str] = []
 				for re in res:
 					re = re.split(" ")
 					if len(re) == 1:
 						all_combos.extend(patterns[re[0]])
 					else:
-						for x in patterns[re[0]]:
-							for y in patterns[re[1]]:
-								all_combos.append(f"{x}{y}")
-						all_combos = list(OrderedDict.fromkeys(all_combos))
+						all_combos = list(
+							OrderedDict.fromkeys(
+								all_combos
+								+ [f"{x}{y}" for x, y in product(patterns[re[0]], patterns[re[1]])]
+							)
+						)
 
 				patterns[rule[0]] = all_combos
 
+	# START PART 1
+	print(len([test for test in tests if test in patterns["0"]]))
+	# END PART 1 START PART 2
 	print(len([test for test in tests if check(test, patterns)]))
+
+
+# END PART 2
 
 
 if __name__ == "__main__":
