02cf6a051d9629479df7d5a6d308153466563de6
Thomas Voss <thomasvoss@live.com>
2021-12-08 01:06:09 +0100

    Remove unused import and minor refactor

--- 2021/07/puzzles.py
+++ 2021/07/puzzles.py
@@ -1,26 +1,17 @@
 #!/usr/bin/env python3
 
-import math
-
-
-def gauss_sum(n: int) -> int:
-	# START PART 1
-	return n
-	# END PART 1 START PART 2
-	return n * (n + 1) // 2
-	# END PART 2
-
 
 def main() -> None:
 	with open("input", "r", encoding="utf-8") as f:
 		data = list(map(int, f.read().split(",")))
 
-	print(
-		min(
-			sum(gauss_sum(abs(i - data[j])) for j in range(len(data)))
-			for i in range(min(data), max(data) + 1)
-		)
-	)
+	# START PART 1
+	gauss_sum = lambda n: n
+	# END PART 1 START PART 2
+	gauss_sum = lambda n: n * (n + 1) // 2
+	# END PART 2
+
+	print(min(sum(gauss_sum(abs(i - n)) for n in data) for i in range(min(data), max(data) + 1)))
 
 
 if __name__ == "__main__":
