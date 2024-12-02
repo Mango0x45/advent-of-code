bf7c30c0e5f4d54efb8b9c679294dbd3238d563b
Thomas Voss <mail@thomasvoss.com>
2024-12-02 22:37:01 +0100

    Slight refactor

--- 2024/01/puzzle-1.el
+++ 2024/01/puzzle-1.el
@@ -1,14 +1,13 @@
 (defun solve (input-file)
-  (let* ((nums (with-temp-buffer
-                 (insert-file-contents-literally input-file)
-                 (goto-char (point-min))
-                 (save-excursion
-                   (insert ?\[)
-                   (goto-char (point-max))
-                   (insert ?\]))
-                 (read (current-buffer))))
-         (num-count (length nums)))
-    (cl-loop for i from 0 below num-count
+  (let ((nums (with-temp-buffer
+                (insert-file-contents-literally input-file)
+                (goto-char (point-min))
+                (save-excursion
+                  (insert ?\[)
+                  (goto-char (point-max))
+                  (insert ?\]))
+                (read (current-buffer)))))
+    (cl-loop for i from 0 below (length nums)
              if (cl-evenp i)
                collect (aref nums i) into xs
              else
