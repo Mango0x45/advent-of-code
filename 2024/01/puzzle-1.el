225cbc55a4992e3b656129fe75e51a139bee2d07
Thomas Voss <mail@thomasvoss.com>
2024-12-02 22:33:06 +0100

    Add an Emacs Lisp solution

---
+++ 2024/01/puzzle-1.el
@@ -0,0 +1,21 @@
+(defun solve (input-file)
+  (let* ((nums (with-temp-buffer
+                 (insert-file-contents-literally input-file)
+                 (goto-char (point-min))
+                 (save-excursion
+                   (insert ?\[)
+                   (goto-char (point-max))
+                   (insert ?\]))
+                 (read (current-buffer))))
+         (num-count (length nums)))
+    (cl-loop for i from 0 below num-count
+             if (cl-evenp i)
+               collect (aref nums i) into xs
+             else
+               collect (aref nums i) into ys
+             finally return (thread-last
+                              (seq-mapn #'- (sort xs) (sort ys))
+                              (mapcar #'abs)
+                              (apply #'+)))))
+
+(message "%d" (solve "input"))
