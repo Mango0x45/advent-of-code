8414d540e9032f5f4b294da8329c82f37ce3147a
Thomas Voss <mail@thomasvoss.com>
2024-12-16 04:04:17 +0100

    Use ~d instead of ~a

--- 2019/05/puzzles.lisp
+++ 2019/05/puzzles.lisp
@@ -2,7 +2,7 @@
 
 (load "../interpreter.lisp")
 
-(format t "~a~%" (car (last (intcode:run (intcode:parse "input")
+(format t "~d~%" (car (last (intcode:run (intcode:parse "input")
                                          ;; START PART 1
                                          '(1)
                                          ;; END PART 1 START PART 2
