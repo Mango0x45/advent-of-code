5b14deaca70a8bda4093a2897d304b80c860acf0
Thomas Voss <mail@thomasvoss.com>
2024-12-18 14:20:52 +0100

    Simplify

--- 2019/05/puzzles.lisp
+++ 2019/05/puzzles.lisp
@@ -3,15 +3,14 @@
 (load "../interpreter.lisp")
 
 (defun main (filename)
-  (let ((stdin (make-string-input-stream
-                ;; START PART 1
-                "1"
-                ;; END PART 1 START PART 2
-                "5"
-                ;; END PART 2
-                ))
-        (stdout (make-string-output-stream)))
-    (intcode:run (intcode:parse "input") stdin stdout)
+  (let ((stdout (make-string-output-stream)))
+    (intcode:run (intcode:parse "input") nil stdout :initial-arguments
+                 ;; START PART 1
+                 '(1)
+                 ;; END PART 1 START PART 2
+                 '(5)
+                 ;; END PART 2
+                 )
     (parse-integer (last-line (get-output-stream-string stdout)))))
 
 (defun last-line (string)
